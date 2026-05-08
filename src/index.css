30e05d3f640bfb59f49eec26b4122249b8b61c36
mathteacher111 <khayley843@gmail.com>
2026-05-08 13:43:37 +0900

    Add files via upload

---
+++ src/index.css
@@ -0,0 +1,17 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+html, body, #root {
+  min-height: 100%;
+}
+
+body {
+  margin: 0;
+  -webkit-tap-highlight-color: transparent;
+  overscroll-behavior: none;
+}
+
+button, input {
+  font-family: inherit;
+}
