5688308c25b407a34958a7eeb08381d6ca083873
mathteacher111 <khayley843@gmail.com>
2026-05-08 14:14:55 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ComplexNumberGamePrototype from './App'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ComplexNumberGamePrototype />
   </React.StrictMode>
-);
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {});
-  });
-}
+)
